9132ef87c7b0ec96e46e18055a8a47aa04b3c4ae
Sagvi <91565408+Sagvi@users.noreply.github.com>
2021-10-11 21:23:14 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,11 +1,12 @@
 const { Client, Intents } = require('discord.js');
 const puppeteer = require('puppeteer');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { token } = require('./config.json');
 
-client.login("ODk3MTY4MTA5Mzk4NTQwMzQ4.YWRu6Q.W_SgrUJLVp1nB8oT8-eIwYlrdXw")
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
 client.on('message', msg => {
   if (msg.content === '?uruchom') {
     msg.channel.send('Czekaj');
@@ -84,6 +85,8 @@ client.on('message', msg => {
     }
   }
 });
+
+client.login(token);
 
 function sleep(ms) {
     return new Promise((resolve) => {
