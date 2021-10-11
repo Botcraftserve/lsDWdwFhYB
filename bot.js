9974b2da3624e5d3fa9e9932721cb95ca1762c9a
Sagvi <91565408+Sagvi@users.noreply.github.com>
2021-10-11 21:38:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,7 +60,9 @@ client.on('message', msg => {
       if (msg.content === '?status') {
         msg.channel.send('Czekaj');
         (async () => {
-          const browser = await puppeteer.launch({ headless: true });
+          const browser = await puppeteer.launch({
+           ignoreDefaultArgs: ['--disable-extensions'],
+          });
           const page = await browser.newPage();
           await page.goto('https://craftserve.pl/login');
           await page.type('input[name=email]', 'hojowie@gmail.com', {delay: 20})
